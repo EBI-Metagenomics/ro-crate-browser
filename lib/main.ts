d9710f6df3027d00570f6c30fc190b5ddcdddb04
Aleksandar <arjkvc@gmail.com>
2024-10-16 14:29:47 +0100

    initial commit

---
+++ lib/main.ts
@@ -0,0 +1,13 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+// import RoCrateBrowser from "./components/RoCrateBrowser.tsx";
+
+export { RoCrateBrowser } from "./components/RoCrateBrowser";
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
